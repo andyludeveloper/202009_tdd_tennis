fd1489a09b9cf464d4c9c7ac137a5dc042bc9a57
AndyLu <andyludeveloper@gmail.com>
2020-09-26 22:35:33 +0800

    refactor production code - add score lookup hash map table - use the lookup table in the score method

--- src/com/andyludeveloper/tdd/Tennis.java
+++ src/com/andyludeveloper/tdd/Tennis.java
@@ -1,17 +1,20 @@
 package com.andyludeveloper.tdd;
 
+import java.util.HashMap;
+
 public class Tennis {
     private int firstPlayerScore;
+    private final HashMap<Integer, String> scoreLookup;
     Tennis(){
         firstPlayerScore = 0;
+        scoreLookup = new HashMap<>();
+        scoreLookup.put(1, "fifteen");
+        scoreLookup.put(2, "thirty");
     }
 
     String score(){
-        if(firstPlayerScore==1){
-            return "fifteen love";
-        }
-        if(firstPlayerScore == 2){
-            return "thirty love";
+        if(firstPlayerScore>0){
+            return scoreLookup.get(firstPlayerScore)+" love";
         }
         return "love all";
     }
